e24f7968a6f59be7c16f04d0c5bef95fb2bb269f
dhse22 <91101417+dhse22@users.noreply.github.com>
2021-11-16 23:35:08 -0500

    Create usuarios.js

---
+++ backend/h3/controllers/usuarios.js
@@ -0,0 +1,31 @@
+const { response, request } = require('express')
+
+const usuariosGet = (req = request, res=response)=> {
+    // const usuario= req.body
+    res.json({
+        msg:'GET'
+    })
+}
+
+const usuariosDelete= (req = request, res=response)=>{
+    // const usuario= req.body
+    res.json({
+        msg:'DEL'
+
+    })
+}
+
+const usuariosPost= (req, res)=>{
+    // const usuario= req.query
+    res.json({
+        msg:'POST'
+    })
+}
+
+const usuariosPut= (req, res)=>{
+    // const usuario= req.query
+    res.json({
+        msg:'PUT'
+
+    })
+}
